98c4e7ee4db02ca9a0aa932ad628e03e4354dca8
Julian Bobbett (DHCW - Software Development) <Julian.Bobbett@wales.nhs.uk>
2026-04-20 15:02:34 +0100

    Add OpenMultipleConnectionsWithEnter user setting
    Introduced a new user setting, OpenMultipleConnectionsWithEnter, to control whether pressing Enter opens multiple connections. Updated App.config and Settings.Designer.cs accordingly. Bumped assembly version to 1.78.2.3488. Also updated code generation version comment in Settings.Designer.cs.

--- mRemoteNG/Properties/AssemblyInfo.cs
+++ mRemoteNG/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Resources;
 
 // Compute version values
 
-//Build nr: 3480
+//Build nr: 3488
 
 // General Information
 [assembly: AssemblyTitle("mRemoteNG")]
@@ -23,7 +23,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version information
-[assembly: AssemblyVersion("1.78.2.3480")]
-[assembly: AssemblyFileVersion("1.78.2.3480")]
+[assembly: AssemblyVersion("1.78.2.3488")]
+[assembly: AssemblyFileVersion("1.78.2.3488")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
-[assembly: AssemblyInformationalVersion("1.78.2 (Nightly Build 3480) x64")]
+[assembly: AssemblyInformationalVersion("1.78.2 (Nightly Build 3488) x64")]
